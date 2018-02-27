f28693c81ad922d3e76c8641e95a110aef261f1c
Noahtyr <Noahdavidian@hotmail.dk>
2018-02-27 11:27:47 +0100

    Add more function to the hello file, testing different stuff to see how it works

--- hello.js
+++ hello.js
@@ -1,5 +1,17 @@
 
 
-let message = "Hello World, it's me, eatthatpussy445";
+let message = "Hello World";
+const five = 5;
+const four = 4;
+let text = "";
 
+
+
+for( i = 0; i < 5; i++) {
+
+    text += " The Number is " + i +"\n";
+}
+
+console.log(five * four);
 console.log(message);
+console.log(text);
