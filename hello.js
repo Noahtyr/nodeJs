0ef5002dd2a8254f910e580b1ea3272ff54fe6d2
Noahtyr <Noahdavidian@hotmail.dk>
2018-02-27 11:45:42 +0100

    Made some changes to for-loop, it twerks

--- hello.js
+++ hello.js
@@ -3,15 +3,26 @@
 let message = "Hello World";
 const five = 5;
 const four = 4;
-let text = "";
-
-
-
-for( i = 0; i < 5; i++) {
-
-    text += " The Number is " + i +"\n";
-}
+//let text = "";
+//let under5 ="";
 
 console.log(five * four);
 console.log(message);
-console.log(text);
+//console.log(text);
+
+for( i = 0; i < 10 ; i++) {
+
+    if(i < 5) {
+
+         console.log("This number is low");
+
+    } else {
+
+        console.log("This number is " + i);
+    }
+}
+
+
+
+//text += " The Number is " + i + "\n";
+//under5 +=" This number is low " + "\n";
